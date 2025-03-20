88067e9df64823fb7dfbe38103e4237397c83a19
Kaua-D <kauadamkeleal@gmail.com>
2025-03-20 14:32:05 -0300

    test2

---
+++ C,/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Digite um numero: ");
+    scanf("%d", &a);
+    printf("Digite um numero: ");
+    scanf("%d", &b);
+    printf("Digite um numero: ");
+    scanf("%d", &c);
+
+    if (a < b && a < c) {
+        if (b < c) {
+            printf("%d %d %d\n", a, b, c);
+        } else {
+            printf("%d %d %d\n", a, c, b);
+        }
+    } else if (b < a && b < c) {
+        if (a < c) {
+            printf("%d %d %d\n", b, a, c);
+        } else {
+            printf("%d %d %d\n", b, c, a);
+        }
+    } else if (c < a && c < b) {
+        if (a < b) {
+            printf("%d %d %d\n", c, a, b);
+        } else {
+            printf("%d %d %d\n", c, b, a);
+        }
+    }
+
+    return 0;
+}
